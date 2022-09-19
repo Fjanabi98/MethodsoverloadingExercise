27e28329c394add155d4c55d7d4dc61c38b03d44
Fahad Janabi <Fahad.janabi98@yahoo.com>
2022-09-19 13:15:24 -0400

    MethodsoverlaodingExercise

---
+++ MethodoverloadingExercise/Methods.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodoverloadingExercise
+{
+
+    internal class Methods
+    {
+        public static int Add(int x, int y)
+        {
+            return x + y;
+        }
+
+        public static decimal Add(decimal x, decimal y)
+        {
+            return x + y;
+        }
+
+        public static string Add(int x, int y, bool a)
+        {
+            if (a == true && x + y > 1)
+            {
+                return ($"{x + y}dollars");
+            }
+            else
+
+            {
+                return ($"{x + y}dollar");
+            }
+
+
+        }
+    }
+}
+    
---
+++ MethodoverloadingExercise/Program.cs
@@ -0,0 +1,10 @@
+﻿using MethodoverloadingExercise;
+
+//var answer = Methods.Add(2,2);
+//Console.WriteLine(answer);
+
+//var answer = Methods.Add(1.99m,2.0001m);
+//Console.WriteLine(answer);
+
+var answer = Methods.Add(5,5,true );
+Console.WriteLine(answer);
